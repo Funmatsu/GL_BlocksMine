bb9a33f513fda860cb58352390535e4ed38e2955
AndyGabrielDev <andygabrielordyhonla@gmail.com>
2026-06-27 23:38:58 -0400

    Daily commit - 9 : " Playable\

--- gameRenderer.h
+++ gameRenderer.h
@@ -241,8 +241,8 @@ public:
 
             directionalShadowPass(&mainLight, model); //binds base shader shaders[0] as well
             glUniform3f(glGetUniformLocation(shaders[0]->getShaderId(), "camPos"), firstCamera.getPosition().x, firstCamera.getPosition().y, firstCamera.getPosition().z);
-            glUniform1f(glGetUniformLocation(shaders[0]->getShaderId(), "fogStart"), 0.7 * CHUNK_SIZE * renderDistance);
-            glUniform1f(glGetUniformLocation(shaders[0]->getShaderId(), "fogEnd"), 0.73 * CHUNK_SIZE * renderDistance);
+            glUniform1f(glGetUniformLocation(shaders[0]->getShaderId(), "fogStart"), 0.72 * CHUNK_SIZE * renderDistance);
+            glUniform1f(glGetUniformLocation(shaders[0]->getShaderId(), "fogEnd"), 0.75 * CHUNK_SIZE * renderDistance);
 
             mainLight.getShadowMap()->read(GL_TEXTURE1);
             glUniform1i(glGetUniformLocation(shaders[0]->getShaderId(), "directionalShadowMap"), 1);
@@ -253,9 +253,11 @@ public:
             Textures[FOLL_TEX]->useTexture(GL_TEXTURE4);
             glUniform1i(glGetUniformLocation(shaders[0]->getShaderId(), "grassColorTexture"), 4);
 
-            isolateWorld();
-			scheduleMeshWorld();
-			renderWorld();
+            //isolateWorld();
+			//scheduleMeshWorld();
+			//renderWorld();
+
+            render();
 
             headPos = firstCamera.getPosition(), headFront = firstCamera.getFront();
             lookBlock = getBlockAt(lookingAtBlock());
@@ -829,14 +831,104 @@ public:
 
     void renderWorld() {
         vec3 playerPos = firstCamera.getPosition() / vec3(CHUNK_SIZE, 1, CHUNK_SIZE);
+        int dirs[] = { -1, 1, 0, 0,   0, 0, -1, 1 };
 
         for (auto it = world.chunkData.begin(); it != world.chunkData.end(); it++) {
             auto& chunk = it->second;
             ivec2 coords = chunk->coords();
-                        
+            
+            if (chunk->getDirty()) {
+                if ((chunk->neighboursPresent & 0x1E) != 0x1E) {
+                    for (int i = 0; i < 4; i++) {
+                        ivec2 chcrds = coords + ivec2(dirs[i], dirs[i + 4]);
+                        if (world.chunkData.count(pack(chcrds)) > 0) {
+                            chunk->neighboursPresent |= (1 << (i + 1));
+                        }
+                    }
+                }
+                if (chunk->neighboursPresent == 0x1E) { // 1 1110 
+                    chNeighPack* chunkochunks = new chNeighPack();
+                    chunkochunks->coords = chunk->coord;
+                    memcpy(chunkochunks->block_data.data(), chunk->block_data.data(), CHUNK_VOLUME);
+                    for (int i = 0; i < 4; i++) {
+                        ivec2 chcrds = coords + ivec2(dirs[i], dirs[i + 4]);
+                        uint idxcrds = pack(chcrds);
+                        if (world.chunkData.count(idxcrds)) {
+                            auto& ch = world.chunkData.at(idxcrds);
+                            memcpy(chunkochunks->neighbour_data[i].data(), ch->block_data.data(), CHUNK_VOLUME);
+                        }
+                    }
+                    {
+                        std::lock_guard<std::mutex> lock(chunkUpdateRequestMutex);
+                        chunkCleanupQueue.push(chunkochunks);
+                    }
+                    chunkUpdateCV.notify_one();
+                    chunk->setAsClean();
+                }
+            }
+
             vec3 center = vec3((coords.x + 0.5) * CHUNK_SIZE, playerPos.y, (coords.y + 0.5) * CHUNK_SIZE);
             if (sphereInFrustum(center, CHUNK_SIZE * 2))
                 chunk->mesh->renderMesh();
+        }
+    }
+
+    void render(){
+        static int tps = 0; //tick per sec
+        vec3 playerPos = firstCamera.getPosition() / vec3(CHUNK_SIZE, 1, CHUNK_SIZE);
+        vec2 _2dPlPosHi = vec2(playerPos.x, playerPos.z) + float(renderDistance),
+            _2dPlPosLo = vec2(playerPos.x, playerPos.z) - float(renderDistance);
+
+        auto it = world.chunkData.begin();
+        while (it != world.chunkData.end()) {
+            auto& chunk = it->second;
+            ivec2 coords = chunk->coords();
+
+            int dirs[] = { -1, 1, 0, 0,   0, 0, -1, 1 };
+
+            if ((coords.x <= _2dPlPosLo.x || coords.x >= _2dPlPosHi.x) ||
+                (coords.y <= _2dPlPosLo.y || coords.y >= _2dPlPosHi.y))
+            {
+                chunkCoords.erase((chunk->coord));
+                it = world.chunkData.erase(it);
+                continue;
+            }
+
+            if (chunk->getDirty()) {
+                if ((chunk->neighboursPresent & 0x1E) != 0x1E) {
+                    for (int i = 0; i < 4; i++) {
+                        ivec2 chcrds = coords + ivec2(dirs[i], dirs[i + 4]);
+                        if (world.chunkData.count(pack(chcrds)) > 0) {
+                            chunk->neighboursPresent |= (1 << (i + 1));
+                        }
+                    }
+                }
+                if (chunk->neighboursPresent == 0x1E) { // 1 1110 
+                    chNeighPack* chunkochunks = new chNeighPack();
+                    chunkochunks->coords = chunk->coord;
+                    memcpy(chunkochunks->block_data.data(), chunk->block_data.data(), CHUNK_VOLUME);
+                    for (int i = 0; i < 4; i++) {
+                        ivec2 chcrds = coords + ivec2(dirs[i], dirs[i + 4]);
+                        uint idxcrds = pack(chcrds);
+                        if (world.chunkData.count(idxcrds)) {
+                            auto& ch = world.chunkData.at(idxcrds);
+                            memcpy(chunkochunks->neighbour_data[i].data(), ch->block_data.data(), CHUNK_VOLUME);
+                        }
+                    }
+                    {
+                        std::lock_guard<std::mutex> lock(chunkUpdateRequestMutex);
+                        chunkCleanupQueue.push(chunkochunks);
+                    }
+                    chunkUpdateCV.notify_one();
+                    chunk->setAsClean();
+                }
+            }
+
+            vec3 center = vec3((coords.x + 0.5) * CHUNK_SIZE, playerPos.y, (coords.y + 0.5) * CHUNK_SIZE);
+            if (sphereInFrustum(center, playerPos.y/2))
+                chunk->mesh->renderMesh();
+
+            it++;
         }
     }
 
